7c69e56a3c275860120a2e9588d02cc9aa927873
jerryjenith <98001380+jerryjenith@users.noreply.github.com>
2022-01-19 08:26:51 +0530

    2nd assignment finished

---
+++ assignment 2.py
@@ -0,0 +1,26 @@
+print("hi its time to learn")
+name='tom'
+print(name)
+
+print('hlo ' +name)
+age = 25
+dep = 'CSE'
+print(name,age,dep)
+print('my name is',name,'iam',age,'i belongs to',dep)
+print("my name is : %s iam : %s i belongs to : %s" %\
+      (name,age,dep))
+print("my name is : {} iam : {} i belongs to : {}" .format(name ,age ,dep))
+fire = list()
+fire.append(25)
+fire.append('hello world')
+fire.append([3,6,2,7])
+print(fire)
+tuple_a =(5,6,2,1,1,5,9)
+print(tuple_a)
+print(tuple_a.count(5))
+print(tuple_a.index(9))
+info = dict()
+info['name']='welome bro'
+print(info)
+
+
