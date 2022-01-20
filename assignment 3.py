6b69fea6ffa4ddf06b5508872eb95bdc40bd6500
jerryjenith <98001380+jerryjenith@users.noreply.github.com>
2022-01-20 08:19:48 +0530

    Assignmnet 3 completed

---
+++ assignment 3.py
@@ -0,0 +1,9 @@
+com=list()
+name=input("enter your name")
+age=int(input("enter your age"))
+address=input("enter your address")
+qualification=input("enter your Qua")
+marks=int(input("enter your marks"))
+print([f"my name : {name}, age : {age},  address: {address}, Qua :{qualification}, marks : {marks}"])
+
+
